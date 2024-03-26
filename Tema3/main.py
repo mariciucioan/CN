875d5f7e30d7e46448a0a47fd6474931641f5279
mariciucioan <79077425+mariciucioan@users.noreply.github.com>
2024-03-26 13:50:58 +0200

    tema3

--- Tema3/main.py
+++ Tema3/main.py
@@ -1,69 +1,152 @@
 import sys
-
 import numpy as np
 
-n = 3
+def data_init(exercitiu6=False, n=3):
+    if exercitiu6:
+        A = np.random.rand(n, n)
+        s = np.random.rand(n)
+        return A, s
+    else:
+        A = [[0, 0, 4],
+             [1, 2, 3],
+             [0, 1, 2]]
+        s = [3, 2, 1]
+
+    return A, s
+
 epsilon = sys.float_info.epsilon
-A = [[0, 0, 4],
-     [1, 2, 3],
-     [0, 1, 2]]
-s = [3, 2, 1]
+A, s = data_init(exercitiu6=True, n=50)
 
 
-def exercitiu1(A, s):
+def exercitiu1(A, s, log=True):
     n = len(s)
     b = np.zeros(n)
     for j in range(n):
         for i in range(n):
             b[i] += s[j] * A[i][j]
 
+    if log:
+        print("Exercitiu 1:\n")
+        print("Valoare b =", b)
+        print("\n----------------")
     return b
 
 
-b = exercitiu1(A, s)
-print("----------------\nExercitiu 1\n----------------")
-print("Valoare b =", b)
+def exercitiu2(A, log=True):
+    Q, R = householder_qr(A)
 
-
-def householder(v):
-    v = np.asarray(v, dtype=float)
-    n = len(v)
-    norm_v = np.linalg.norm(v)
-    if norm_v == 0:
-        return np.eye(n)
-
-    v = v.reshape((n, 1))
-    v_normalized = v / norm_v
-    P = np.eye(n) - 2 * np.dot(v_normalized, v_normalized.T)
-
-    return P
-
-
-def qr_decomposition(A):
-    A = np.asarray(A, dtype=float)
-    R = np.copy(A)
-    Q = np.eye(n)
-
-    for i in range(n):
-        x = R[i:, i]
-        e = np.zeros_like(x)
-        e[0] = np.linalg.norm(x)
-        v = x - e
-        P = householder(v)
-
-        R[i:, i:] = np.dot(P, R[i:, i:])
-        Q[:, i:] = np.dot(Q[:, i:], P)
+    if log:
+        print("Exercitiu 2:\n")
+        print("Q:")
+        print(Q)
+        print("R:")
+        print(R)
+        print("Q * R:")
+        print(np.dot(Q, R))
+        print("\n----------------")
 
     return Q, R
 
 
-print("\n----------------\nExercitiu 2\n----------------")
+def exercitiu3_prim(A_param, b_param, householder):
+    if householder:
+        Q_local, R_local = exercitiu2(A_param, log=False)
+    else:
+        Q_local, R_local = np.linalg.qr(A_param)
 
-Q, R = qr_decomposition(A)
-print("Matricea Q:")
-print(Q)
-print("\nMatricea R:")
-print(R)
+    return solve_linear_system(Q_local, R_local, b_param)
 
-print("\nMatricea Q*R:")
-print(np.dot(Q, R))
+
+def exercitiu3():
+    print("Exercitiu 3:\n")
+    print("numpy QR:")
+    print(x_QR)
+    print("householder QR:")
+    print(x_householder)
+    calculate_and_print_norm(x_QR - x_householder, "x_QR - x_householder")
+    print("\n----------------")
+
+
+def exercitiu4():
+    print("Exercitiu 4:\n")
+
+    calculate_and_print_norm(A @ x_householder - b, "A*x_householder - b")
+    calculate_and_print_norm(A @ x_QR - b, "A*x_QR - b")
+    calculate_and_print_norm((x_householder - s) / np.linalg.norm(s), "(x_householder-s)/(norma euclidiana a lui s)")
+    calculate_and_print_norm((x_QR - s) / np.linalg.norm(s), "(x_QR - s)/(norma euclidiana a lui s)")
+
+    print("\n----------------")
+
+
+def exercitiu5():
+    print("Exercitiu 5:\n")
+    A_inv_householder = calculate_matrix_inverse(Q, R)
+    A_inv_numpy = np.linalg.inv(A)
+
+    calculate_and_print_norm(A_inv_householder - A_inv_numpy, "A_inv_householder - A_inv_numpy", ordin=np.inf)
+    print("\n----------------")
+
+
+def householder_qr(A):
+    n = len(A)
+    R = np.copy(A).astype(float)
+    Q = np.eye(n)
+
+    for r in range(n - 1):
+        sigma = np.sum(R[r:, r] ** 2)
+        if sigma <= epsilon:
+            break
+        k = np.sqrt(sigma)
+        if R[r, r] > 0:
+            k = -k
+        beta = sigma - k * R[r, r]
+        u = np.zeros(n)
+        u[r] = R[r, r] - k
+        u[r + 1:] = R[r + 1:, r]
+
+        # Update R
+        for j in range(r, n):
+            gamma = np.sum(u[r:] * R[r:, j]) / beta
+            R[r:, j] -= gamma * u[r:]
+
+        # Update Q
+        for j in range(n):
+            gamma = np.sum(u * Q[:, j]) / beta
+            Q[:, j] -= gamma * u
+
+    return Q.T, R
+
+
+def solve_linear_system(Q, R, b):
+    Qt_b = np.dot(Q.T, b)
+
+    n = len(b)
+    x = np.zeros(n)
+
+    for i in range(n - 1, -1, -1):
+        x[i] = (Qt_b[i] - np.dot(R[i, i + 1:], x[i + 1:])) / R[i, i]
+
+    return x
+
+
+def calculate_and_print_norm(val, str, ordin=2):
+    type = "absoluta"
+    if ordin == 2:
+        type = "euclidiana"
+
+    print(f"Norma {type} {str} = {np.linalg.norm(val, ord=ordin)}")
+
+
+def calculate_matrix_inverse(Q, R):
+    R_inv = np.linalg.inv(R)
+    A_inv = np.dot(R_inv, Q.T)
+    return A_inv
+
+
+b = exercitiu1(A, s)
+Q, R = exercitiu2(A)
+x_QR = exercitiu3_prim(A, b, householder=False)
+x_householder = exercitiu3_prim(A, b, householder=True)
+exercitiu3()
+exercitiu4()
+exercitiu5()
